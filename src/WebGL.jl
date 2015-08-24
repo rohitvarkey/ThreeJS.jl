f89a42c2d4e3b10dbb947b7e57144b8b32d4ade2
Rohit Varkey Thankachan <rohitvarkey@gmail.com>
2015-08-24 18:10:03 +0530

    Basic setup done.

--- src/WebGL.jl
+++ src/WebGL.jl
@@ -1,5 +1,22 @@
 module WebGL
 
-# package code goes here
+import Patchwork
+import Patchwork.Elem
+using Compat
+
+export initscene
+
+"Initiates a three-js scene"
+function initscene(w::String="100%", h::String="600px")
+    Elem(:div, style=@compat Dict(:width=>w, :height=>h)) <<
+        Elem(:"three-js")
+end
+
+if isdefined(Main, :IJulia)
+    display(
+        MIME"text/html"(),
+        "<link rel=import href=/static/components/compose3d/bower_components/three-js/three-js.html>"
+    )
+end
 
 end # module
