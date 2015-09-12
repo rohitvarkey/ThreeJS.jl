ccba3acb483c931517338342afb9f3426d36d71d
Rohit Varkey Thankachan <rohitvarkey@gmail.com>
2015-09-12 18:59:02 +0530

    Added rotating cube example.

---
+++ examples/rotatingcube.jl
@@ -0,0 +1,28 @@
+#Run this in Escher to see a rotating cube in the browser.
+
+import ThreeJS 
+
+main(window) =  begin
+    push!(window.assets,("ThreeJS","threejs"))
+    eventloop = every(1/60)
+    rx = 0.0
+    ry = 0.0
+    rz = 0.0
+    lift(eventloop) do _
+        rx += 0.5
+        ry += 0.5
+        rz += 0.5
+        ThreeJS.outerdiv() << 
+                (ThreeJS.initscene() <<
+                    [
+                        ThreeJS.mesh(0.0, 0.0, 0.0; rx=rx, ry=ry, rz=rz) << 
+                        [
+                            ThreeJS.box(5.0, 5.0, 5.0), ThreeJS.material(Dict(:kind=>"phong",:color=>"red"))
+                        ],
+                        ThreeJS.pointlight(10.0, 10.0, 10.0),
+                        ThreeJS.pointlight(-10.0, -10.0, -10.0),
+                        ThreeJS.camera(0.0, 0.0, 20.0)
+                    ]
+                )
+        end
+end
