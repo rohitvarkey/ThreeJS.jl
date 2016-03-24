12095feea1b431cc2a6e993a3d56605443499c5d
Rohit Varkey Thankachan <rohitvarkey@gmail.com>
2016-03-24 14:45:26 +0530

    [ci skip]Change images.

--- examples/texture.jl
+++ examples/texture.jl
@@ -10,12 +10,12 @@ main(window) =  begin
             [
                 #Any accessible URL can be provided to the texture attribute to render it as texture
                 ThreeJS.box(2.0, 2.0, 2.0),
-                ThreeJS.material(Dict(:kind=>"texture",:texture=>"assets/crate.jpg"))
+                ThreeJS.material(Dict(:kind=>"texture",:texture=>"assets/crate.png"))
             ],
             ThreeJS.mesh(3.0, 3.0, 3.0) << 
             [
                 ThreeJS.plane(2.0, 2.0),
-                ThreeJS.material(Dict(:kind=>"texture",:texture=>"assets/Doge.jpg"))
+                ThreeJS.material(Dict(:kind=>"texture",:texture=>"assets/doge.png"))
             ],
             ThreeJS.pointlight(10.0, 10.0, 10.0),
             ThreeJS.camera(0.0, 0.0, 20.0)
