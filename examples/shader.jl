e20e3dcabe25c0df6d8087e96e452d413fcf774c
Shashi Gowda <shashigowda91@gmail.com>
2016-09-05 09:56:28 +0530

    update example

--- examples/shader.jl
+++ examples/shader.jl
@@ -4,6 +4,23 @@ t = every(1.0)
 
 using Reactive
 
+
+vert = """
+        varying vec2 v_uv;
+
+        void main() {
+          v_uv = uv;
+          gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+        }
+        """
+
+frag = """
+        varying vec2 v_uv;
+        uniform float alpha;
+
+        void main() { gl_FragColor = vec4(v_uv, 0.0, alpha); }
+        """
+
 main(window) =  begin
     push!(window.assets,("ThreeJS","threejs"))
     push!(window.assets,"widgets")
@@ -20,10 +37,10 @@ main(window) =  begin
             ThreeJS.mesh(0.0, 0.0, 0.0) <<
             [
                 ThreeJS.plane(8.0, 8.0),
-                ThreeJS.shadermaterial(uniforms=Dict(:alpha=>Dict(:type=>"f", :value=>n), :beta=>Dict(:type=>"f", :value=>0.1)),
-                    defines=PropHook("NestedUpdate", Dict(:a=>2)),
-                    vertexshader=open(readall, "assets/shader.vert", "r"),
-                    fragmentshader=open(readall, "assets/shader.frag", "r"))
+                ThreeJS.shadermaterial(
+                    uniforms=PropHook("escher-property-hook", Dict(:alpha=>Dict(:type=>"f", :value=>1-n/10))),
+                    vertexshader=vert,
+                    fragmentshader=frag,)
             ],
             ThreeJS.pointlight(10.0, 10.0, 10.0),
             ThreeJS.camera(0.0, 0.0, 20.0)
