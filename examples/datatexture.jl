4f27f7312ec62cde082945dc303e60948b15dfe4
Irwin Zaid <hi@irwinzaid.com>
2016-09-12 21:44:05 +0100

    Used Iterator.cycle instead in datatexture example

--- examples/datatexture.jl
+++ examples/datatexture.jl
@@ -1,55 +1,29 @@
 import ThreeJS
 import FileIO: load
-import Iterators: cycle
 import Reactive: every
 
 import Images
-#myfilename = "assets/doge.png"
-
-#using FileIO
-#obj = load(myfilename)
 
 ascent = load("assets/ascent.png")
 
 a = Images.separate(ascent)
 a = a.data
 b = reinterpret(UInt8, a)
+
+println(typeof(b))
 #println(b)
 #a = img.data
 #b = reinterpret(Array{UInt8, 2}, a)
 
-println(typeof(a))
-
-next = 1
-
-images = (b, b[257:512, 257:512], b[355:463, 292:375])
-count = length(images)
-
-#c = cycle(images)
-
-function getnext()
-    global next
-    global images
-
-    println(next)
-    retval = images[next]
-    println(size(retval))
-
-    next += 1
-    if (next > count)
-        next = 1
-    end
-
-    retval
-end
-
-# periodic_leading = sampleon(every10secs, leading)
+images = cycle((b, b[257:512, 257:512], b[355:463, 292:375]))
 
 main(window) =  begin
     push!(window.assets,("ThreeJS","threejs"))
     push!(window.assets,"nested-props")
 
-    map(map(t -> getnext(), every(1))) do a
+    i = start(images)
+    map(map(t -> next(images, i), every(1))) do pair
+        a, i = pair
         ThreeJS.outerdiv() <<
         (ThreeJS.initscene() <<
             [
