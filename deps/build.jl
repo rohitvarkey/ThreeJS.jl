8372e1f57df431b214d1dab319f2b08399f43659
Irwin Zaid <hi@irwinzaid.com>
2016-10-06 17:53:35 +0100

    Updated build.jl script

--- deps/build.jl
+++ deps/build.jl
@@ -1,3 +1,12 @@
+version = "master"
+
+function updatejs()
+    file = "$version.zip"
+
+    download("https://github.com/rohitvarkey/three-js/archive/$file", file)
+    run(`unzip -qq $file`)
+    mv("three-js-$version", joinpath(dirname(@__FILE__), "..", "assets", "three-js"), remove_destination=true)
+end
+
 # Install the bower_components
-cd("../assets")
-run(`bower install rohitvarkey/three-js`)
+mktempdir(dir -> cd(updatejs, dir))
